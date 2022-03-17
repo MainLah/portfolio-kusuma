17c2e9a6820507381f85f26255fb8659a5b6c01e
MainLah <rendrakusumab@gmail.com>
2022-03-17 14:47:51 +0700

    added starback, continued about section

--- About/about.js
+++ About/about.js
@@ -3,11 +3,18 @@ const prev = document.querySelector("#prev");
 const circles = document.querySelectorAll(".circle");
 const progress = document.querySelector(".progress");
 const progressDivided = 100 / (circles.length - 1);
+const text = document.querySelectorAll(".text h3");
 
 let circleCounter = 1;
 let progressCounter = 0;
 next.addEventListener("click", () => {
   circles[circleCounter].classList.add("active");
+
+  text.forEach((text) =>
+    text.classList.replace("text-active", "text-inactive")
+  );
+  text[circleCounter].classList.replace("text-inactive", "text-active");
+
   circleCounter++;
   progressCounter += progressDivided;
   if (progressCounter > 100) {
@@ -25,6 +32,12 @@ next.addEventListener("click", () => {
 prev.addEventListener("click", () => {
   circleCounter--;
   circles[circleCounter].classList.remove("active");
+
+  text.forEach((text) =>
+    text.classList.replace("text-active", "text-inactive")
+  );
+  text[circleCounter - 1].classList.replace("text-inactive", "text-active");
+
   progressCounter -= progressDivided;
   if (progressCounter < 0) {
     progressCounter = 0;
--- home.js
+++ home.js
@@ -2,6 +2,12 @@ const navButton = document.querySelector(".container-nav");
 const content = document.querySelector(".container");
 const navItems = document.querySelectorAll(".nav-icons h3");
 
+window.onload = () => {
+  if (screen.width > 500) {
+    navItems[navItems.length - 1].style.display = "inline-block";
+  }
+};
+
 navButton.addEventListener("click", () => {
   content.classList.toggle("show-nav");
   navItems.forEach((e) => {
